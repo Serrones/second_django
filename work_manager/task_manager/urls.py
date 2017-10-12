6bd28a30413df86ac76875c6ac10082c24fb04e7
serrones <fabioserrones@gmail.com>
2017-10-12 14:09:02 -0300

    Chapter 6 - Second Part

--- work_manager/task_manager/urls.py
+++ work_manager/task_manager/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url (r'^$', views.hello_page, name='home'),
     url (r'^serrones$', views.hello_serrones, name='RaD'),
     url(r'^connection$', views.connection_page, name='connection'),
+    url(r'^projects$', views.projects_page, name='projects'),
+    url(r'^project-detail-(?P<pk>\d+)$', views.project_detail, name="project_detail"),
 ]
--- work_manager/task_manager/views.py
+++ work_manager/task_manager/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project, Task, Dev_Work_Task, Developer, Supervisor, User_Profile
+from django.utils import timezone
 
 # Create your views here.
 
 #View for index page
 def hello_page(request):
+    
+    return render(request, 'task_manager/index.html', {'action' : 'Save relationship'})
+
+#View for index page
+def projects_page(request):
     all_projects = Project.objects.all()
-    return render(request, 'task_manager/index.html', {'action': "Display all projects", 'all_projects': all_projects})
+    return render(request, 'task_manager/projects.html', {'action': "Display all projects", 'all_projects': all_projects})
+
+def project_detail(request, pk):
+    project = Project.objects.get(id=pk)
+    return render(request, 'task_manager/project_detail.html', {'project': project})
+
 #View for personal calling
 def hello_serrones(request):
     return HttpResponse ("Rumo a dominação!!!")
